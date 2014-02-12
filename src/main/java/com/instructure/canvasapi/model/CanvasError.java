675cb5601eebcade4dc42c6aae29bafb95c971d1
Nathan Button <nbutton@instructure.com>
2014-02-12 10:14:32 -0700

    Nathan Button initial code commit

---
+++ src/main/java/com/instructure/canvasapi/model/CanvasError.java
@@ -0,0 +1,76 @@
+package com.instructure.canvasapi.model;
+
+/**
+ * Created by Nathan Button on 1/21/14.
+ */
+public class CanvasError {
+    private String status;
+    private Error errors;
+    private String message;
+    private String formattedStatus;
+    private String errorMessage;
+
+    public String getStatus(){
+        if (formattedStatus == null) {
+            if (status != null & status.length() > 1) {
+
+                formattedStatus = status.substring(0, 1).toUpperCase() + status.substring(1);
+
+            } else {
+                formattedStatus = "";
+            }
+        }
+        return formattedStatus;
+    }
+
+    public String getMessage(){
+        if(message != null){
+            return message;
+        }
+
+        return "";
+    }
+
+    public Error getErrors(){
+        return errors;
+    }
+
+    @Override
+    public String toString() {
+        if (errorMessage == null) {
+            if (getStatus().equals("") && errors != null && errors.getMessage().equals("")) {
+
+                errorMessage = getStatus() + ": " + getErrors().getMessage();
+
+
+            }
+        } else {
+            errorMessage = "";
+        }
+
+        return errorMessage;
+    }
+
+
+    public class Error {
+        private String message;
+        private String formattedMessage;
+
+        public String getMessage() {
+            if (formattedMessage == null) {
+                if (message != null && message.length() > 1) {
+                    formattedMessage = message.substring(0, 1).toUpperCase() + message.substring(1) + ".";
+                } else {
+                    formattedMessage = "";
+                }
+            }
+            return formattedMessage;
+        }
+
+
+        @Override
+        public String toString() {
+            return getMessage();
+        }
+    }
+}
