73fab981a67aa4d7db08c2f3edc03a54bf5aad8e
Brady Larson <brady@instructure.com>
2015-02-13 16:24:10 -0700

    Modified AccountNotificationAPI to get the user's own notifications and modified the AccountNotifications to match the JSON that is returned.

--- src/main/java/com/instructure/canvasapi/api/AccountNotificationAPI.java
+++ src/main/java/com/instructure/canvasapi/api/AccountNotificationAPI.java
@@ -15,11 +15,11 @@ public class AccountNotificationAPI {
 
     public interface AccountNotificationInterface {
 
-        @GET("/accounts/{account_id}/users/{user_id}/account_notifications")
-        void getAccountNotifications(@Path("account_id") long account_id, @Path("user_id") long user_id, CanvasCallback<AccountNotification[]> callback);
+        @GET("/accounts/self/users/self/account_notifications")
+        void getAccountNotifications(CanvasCallback<AccountNotification[]> callback);
 
-        @DELETE("/accounts/{account_id}/users/{user_id}/account_notifications/{account_notification_id}")
-        void deleteAccountNotification(@Path("account_id") long account_id, @Path("user_id") long user_id, @Path("account_notification_id") long account_notification_id, CanvasCallback<AccountNotification> callback);
+        @DELETE("/accounts/self/users/self/account_notifications/{account_notification_id}")
+        void deleteAccountNotification(@Path("account_notification_id") long account_notification_id, CanvasCallback<AccountNotification> callback);
 
     }
 
@@ -33,15 +33,15 @@ public class AccountNotificationAPI {
         return restAdapter.create(AccountNotificationInterface.class);
     }
 
-    public static void getAccountNotifications(long accountId, long userId, final CanvasCallback<AccountNotification[]> callback) {
+    public static void getAccountNotifications(final CanvasCallback<AccountNotification[]> callback) {
         if (APIHelpers.paramIsNull(callback)) { return; }
 
-        buildInterface(callback, null).getAccountNotifications(accountId, userId, callback);
+        buildInterface(callback, null).getAccountNotifications(callback);
     }
 
-    public static void deleteAccountNotification(long accountId, long userId, long accountNotificationId, CanvasCallback<AccountNotification> callback) {
+    public static void deleteAccountNotification(long accountNotificationId, CanvasCallback<AccountNotification> callback) {
         if (APIHelpers.paramIsNull(callback)) { return; }
 
-        buildInterface(callback, null).deleteAccountNotification(accountId, userId, accountNotificationId, callback);
+        buildInterface(callback, null).deleteAccountNotification(accountNotificationId, callback);
     }
 }
