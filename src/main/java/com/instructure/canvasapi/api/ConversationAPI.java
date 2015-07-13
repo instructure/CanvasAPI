c32639557eee9964d94c603afc56c35f8229ea07
Dan Willoughby <dwilloughby@instructure.com>
2015-07-13 10:13:00 -0600

    change method to public so cache directory is accessible

--- src/main/java/com/instructure/canvasapi/api/ConversationAPI.java
+++ src/main/java/com/instructure/canvasapi/api/ConversationAPI.java
@@ -52,7 +52,7 @@ public class ConversationAPI {
         return "";
     }
 
-    private static String getFirstPageConversationsCacheFilename(ConversationScope scope){
+    public static String getFirstPageConversationsCacheFilename(ConversationScope scope){
         return "/conversations/" + conversationScopeToString(scope);
     }
 
