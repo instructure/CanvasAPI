c8e6ea99e7ae71f641b401323b62eac8398128de
Matthew Rice <mrice@instructure.com>
2015-10-23 15:46:22 -0600

    Removes some unused per_page, Fixes Masquerading

--- src/main/java/com/instructure/canvasapi/utilities/Masquerading.java
+++ src/main/java/com/instructure/canvasapi/utilities/Masquerading.java
@@ -9,17 +9,13 @@ import com.instructure.canvasapi.model.User;
 import java.io.File;
 
 /**
- * Created by Josh Ruesch on 10/16/13.
- *
- * Copyright (c) 2014 Instructure. All rights reserved.
+ * Copyright (c) 2015 Instructure. All rights reserved.
  */
 public class Masquerading {
 
     private final static String MASQ_PREF_NAME = "masquerading-SP";
     private final static String IS_MASQUERADING = "isMasquerading";
     private final static String MASQUERADE_ID = "masqueradeId";
-
-
 
     //function to add masquerading id to end of query string
     public static String addMasqueradeId(String url, Context context) {
@@ -40,11 +36,11 @@ public class Masquerading {
         return settings.getLong(MASQUERADE_ID,-1);
     }
 
-
     public static boolean isMasquerading(Context context) {
         SharedPreferences settings = context.getSharedPreferences(MASQ_PREF_NAME, 0);
         return settings.getBoolean(IS_MASQUERADING, false);
     }
+
     public static void stopMasquerading(Context context) {
         File cacheDir = new File(context.getFilesDir(), "cache");
         FileUtilities.deleteAllFilesInDirectory(cacheDir);
@@ -57,6 +53,7 @@ public class Masquerading {
         editor.putLong(MASQUERADE_ID, -1);
         editor.apply();
     }
+
     public static void startMasquerading(long masqueradeId, Context context, CanvasCallback<User> masqueradeUser, String domain) {
 
         File cacheDir = new File(context.getFilesDir(), "cache");
@@ -75,7 +72,6 @@ public class Masquerading {
             APIHelpers.setDomain(context, domain);
         }
 
-        UserAPI.getUserById(masqueradeId, masqueradeUser);
-
+        UserAPI.getUserByIdNoCache(masqueradeId, masqueradeUser);
     }
 }
