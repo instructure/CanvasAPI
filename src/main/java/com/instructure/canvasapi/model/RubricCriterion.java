4febd60d72071c4f6f65b9dcc6e29dfeae19da60
Hoa Hoang <hhoang@instructure.com>
2016-01-18 13:20:41 -0700

    Removed unnecessary Log import

--- src/main/java/com/instructure/canvasapi/model/RubricCriterion.java
+++ src/main/java/com/instructure/canvasapi/model/RubricCriterion.java
@@ -2,8 +2,6 @@ package com.instructure.canvasapi.model;
 
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.util.Log;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
